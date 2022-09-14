ca3e0bb3ec7d588bd7bc2724bec76063edeff898
MinhHieuVux <minhhieuvux@gmail.com>
2022-09-14 14:08:53 +0700

    add form messages

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,18 +5,19 @@ class RoomsController < ApplicationController
     @room = Room.new
     @rooms = Room.public_rooms
     @users = User.all_except(current_user)
-    render "index"
+    render 'index'
   end
 
   def show
     @single_room = Room.find(params[:id])
     @room = Room.new
     @rooms = Room.public_rooms
-    @users = User.all_except(current_user)
-    @message = Message.new
-    @messages = @single_room.messages.order(create_at: :asc)
-    render "index"
 
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+
+    @users = User.all_except(current_user)
+    render 'index'
   end
 
   def create
